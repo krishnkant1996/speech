c7b481fe23df7bd8e2acd962ac0b3ad435a33ad8
krishnkant <krishnkant.tiwari@iviewlabs.net>
2020-04-23 13:03:38 +0530

    Updates

---
+++ precache-manifest.d20065b33f202a7eddc3e09e1aad6ff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4044b0c908a53d85c34f44015edb56c5",
+    "url": "/speech/index.html"
+  },
+  {
+    "revision": "b4d4e2b58cf348d3f020",
+    "url": "/speech/static/js/2.a0cfe63d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/speech/static/js/2.a0cfe63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25544bb4613ae63e296c",
+    "url": "/speech/static/js/main.f19080a1.chunk.js"
+  },
+  {
+    "revision": "b70aa5e85845fa16766a",
+    "url": "/speech/static/js/runtime-main.84bc1fd5.js"
+  }
+]);
